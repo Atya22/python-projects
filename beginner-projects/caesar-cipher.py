0410f73d34a217a69328c218458dba92a06531fe
Atya22 <aytaj2002eva@gmail.com>
2024-07-31 12:42:05 +0400

    Caesar-cipher game

--- beginner-projects/caesar-cipher.py
+++ beginner-projects/caesar-cipher.py
@@ -5,8 +5,6 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
-
 def encrypt(plain_text, shift_amount):
     cipher_text = ""
     max_index = len(alphabet) - shift_amount
@@ -27,17 +25,7 @@ def decrypt(cipher_text, shift_amount):
         else:
             decrypt_text += alphabet[index_element + len(alphabet) - shift_amount]
     print(f"The decrypted text is {decrypt_text}")
-    
 
-  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
-  #e.g. 
-  #cipher_text = "mjqqt"
-  #shift = 5
-  #plain_text = "hello"
-  #print output: "The decoded text is hello"
-
-
-#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
 if direction == "decode":
     decrypt(text, shift)
 elif direction == "encode":
