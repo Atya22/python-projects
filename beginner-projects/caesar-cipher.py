1ee763ad782d970fec3358d696bb0f5cdda731c9
Atya22 <aytaj2002eva@gmail.com>
2024-08-02 21:17:29 +0400

    Combine functions into a single function

--- beginner-projects/caesar-cipher.py
+++ beginner-projects/caesar-cipher.py
@@ -5,28 +5,19 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
-    cipher_text = ""
-    max_index = len(alphabet) - shift_amount
-    for i in plain_text:
+def caesar(text, shift_amount, direction):
+    new_text = ""
+    index = len(alphabet) - shift_amount
+    for i in text:
         index_element = alphabet.index(i)
-        if index_element < max_index:
-            cipher_text += alphabet[index_element + shift_amount]
-        else:
-            cipher_text += alphabet[index_element - len(alphabet) + shift_amount]
-
-def decrypt(cipher_text, shift_amount):
-    decrypt_text = ""
-    min_index = len(alphabet) - shift_amount
-    for i in cipher_text:
-        index_element = alphabet.index(i)
-        if index_element < min_index:
-            decrypt_text += alphabet[index_element - shift_amount]
-        else:
-            decrypt_text += alphabet[index_element + len(alphabet) - shift_amount]
-    print(f"The decrypted text is {decrypt_text}")
-
-if direction == "decode":
-    decrypt(text, shift)
-elif direction == "encode":
-    encrypt(text, shift)
+        if direction == "decode":
+            if index_element < index:
+                new_text += alphabet[index_element - shift_amount]
+            else:
+                new_text += alphabet[index_element + len(alphabet) - shift_amount]
+        elif direction == "encode":
+            if index_element < index:
+                new_text += alphabet[index_element + shift_amount]
+            else:
+                new_text += alphabet[index_element - len(alphabet) + shift_amount]
+    print(f"The decrypted text is {new_text}")
